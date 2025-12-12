dbb78da46e8ffab022f3b52ce469533290a54b23
Sevak Avakians <sevakavakians@gmail.com>
2025-12-12 13:57:23 -0500

    chore: bump version to 3.1.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(os.path.join(this_directory, 'requirements.txt'), encoding='utf-8') as
 
 setup(
     name="kato",
-    version="3.0.2",
+    version="3.1.0",
     description="Knowledge Abstraction for Traceable Outcomes - Transparent memory and abstraction for agentic AI systems",
     long_description=long_description,
     long_description_content_type="text/markdown",
